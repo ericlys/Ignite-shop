709196336a8f46f50839710e89607a8e181ff5eb
Ericlys Moreira <ericlysm@gmail.com>
2023-01-11 21:44:19 -0300

    chore: staticprops

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,10 @@
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import Image from "next/image"
 
 import { useKeenSlider } from 'keen-slider/react'
 
 import { stripe } from "../lib/stripe"
 import { HomeContainer, Product } from "../styles/pages/home"
-
-import tshert1 from '../assets/shirts/1.png'
-import tshert2 from '../assets/shirts/2.png'
-import tshert3 from '../assets/shirts/3.png'
 
 import 'keen-slider/keen-slider.min.css'
 import Stripe from "stripe"
@@ -48,7 +44,7 @@ export default function Home({products}: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps= async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -67,6 +63,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2, //2 horas
   }
 }
